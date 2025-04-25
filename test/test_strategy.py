e85b776abfd11d8eae951a0f5fc46907f71c5368
TiwariLaxuu <tiwarilaxuu@gmail.com>
2025-04-25 13:54:28 +0545

    add code

--- test/test_strategy.py
+++ test/test_strategy.py
@@ -18,3 +18,9 @@ def test_sma_strategy():
 
 if __name__ == '__main__':
     test_sma_strategy()
+
+# This test checks if the SMACrossover strategy generates signals correctly.
+# It creates a DataFrame with dummy data and verifies that the output is a dictionary.
+# The test can be run by executing this script directly.
+# The test can be expanded to include more specific checks, such as verifying the contents of the signals.
+
